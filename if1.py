e59abbad664ea35b9b8b34fddabab8802bc91265
Anton Kudryashov <amkudr@mail.ru>
2021-05-19 20:07:44 +0300

    Добавление main

--- if1.py
+++ if1.py
@@ -1,19 +1,22 @@
-age = int(input("Введите ваш возраст "))
-def selection(age):
-    if age < 0:
-        raise ValueError("Введите корректный возраст")
-    if age < 7:
-        return("Вы в детском саду")
-    if age < 19:
-        return("Вы в школе")
-    if age < 24:
-        return("Вы в ВУЗе")
-    if age <66:
-        return("Вы работаете")
-    else:
-        return("Вы на пенсии или умерли")
-escape = selection(age)
-print(escape)
+def main():
+    age = int(input("Введите ваш возраст "))
+    def selection(age):
+        if age < 0:
+            raise ValueError("Введите корректный возраст")
+        if age < 7:
+            return("Вы в детском саду")
+        if age < 19:
+            return("Вы в школе")
+        if age < 24:
+            return("Вы в ВУЗе")
+        if age <66:
+            return("Вы работаете")
+        else:
+            return("Вы на пенсии или умерли")
+    escape = selection(age)
+    print(escape)
+if __name__ == "__main__":
+    main()
 
     
 
--- if2.py
+++ if2.py
@@ -1,13 +1,17 @@
-line1 = input("Введите строку ")
-line2 = input("Введите строку ")
-if type(line1) != str or type(line2) != str:
-    print(0)
-if line1 == line2:
-    print(1)
-elif len(line1)>len(line2):
-    print(2)
-elif line2 == "learn":
-    print(3)
+def main():
+    line1 = input("Введите строку ")
+    line2 = input("Введите строку ")
+    if type(line1) != str or type(line2) != str:
+        print(0)
+    if line1 == line2:
+        print(1)
+    elif len(line1)>len(line2):
+        print(2)
+    elif line2 == "learn":
+        print(3)
+if __name__ == "__main__":
+    main()
+
 
 
 
