ac0d19e032703c5f5d47d3e29806c5866b85a067
Anton Kudryashov <amkudr@mail.ru>
2021-05-19 19:06:30 +0300

    Fixed return function

--- if1.py
+++ if1.py
@@ -3,15 +3,15 @@ def selection(age):
     if age < 0:
         raise ValueError("Введите корректный возраст")
     if age <= 0 and age < 7:
-        print("Вы в детском саду")
+        return("Вы в детском саду")
     if age >= 7 and age <= 18:
-        print("Вы в школе")
+        return("Вы в школе")
     if age >= 18 and age <= 24:
-        print("Вы в ВУЗе")
+        return("Вы в ВУЗе")
     if age >24 and age <=65:
-        print("Вы работаете")
-    #else:
-        #print("Вы скорее всего умерли")
+        return("Вы работаете")
+    else:
+        return("Вы скорее всего умерли")
 escape = selection(age)
 print(escape)
 
